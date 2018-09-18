0cb436cbb8f71fda32d5d4e080f59ed9fce089fa
Per Liedman <per.liedman@dotnetmentor.se>
2018-09-18 23:04:36 +0200

    Initial work extracting objects

--- src/index.js
+++ src/index.js
@@ -18,24 +18,147 @@ module.exports = async path => {
 const parseOcadBuffer = async buffer => new Promise((resolve, reject) => {
   const header = new FileHeader(buffer)
   if (!header.isValid()) {
-    reject(new Error(`Not an OCAD file (invalid header ${header.OcadMark} !== ${0x0cad})`))
+    reject(new Error(`Not an OCAD file (invalid header ${header.ocadMark} !== ${0x0cad})`))
   }
 
-  resolve({})
+  let objects = []
+  let objectIndexOffset = header.objectIndexBlock
+  while (objectIndexOffset) {
+    let objectIndex = new ObjectIndex(buffer, objectIndexOffset)
+    Array.prototype.push.apply(objects, objectIndex.parseObjects())
+
+    objectIndexOffset = objectIndex.nextObjectIndexBlock
+  }
+
+  resolve({
+    header,
+    objects
+  })
 })
 
-class FileHeader {
+class Block {
   constructor (buffer, offset) {
-    offset = offset || 0
+    this.buffer = buffer
+    this.offset = offset || 0
+  }
+
+  readInteger () {
+    const val = this.buffer.readInt32LE(this.offset)
+    this.offset += 4
+    return val
+  }
+
+  readCardinal () {
+    const val = this.buffer.readUInt32LE(this.offset)
+    this.offset += 4
+    return val
+  }
+
+  readSmallInt () {
+    const val = this.buffer.readInt16LE(this.offset)
+    this.offset += 2
+    return val
+  }
+
+  readByte () {
+    const val = this.buffer.readInt8(this.offset)
+    this.offset++
+    return val
+  }
+
+  readWord () {
+    const val = this.buffer.readUInt16LE(this.offset)
+    this.offset += 2
+    return val
+  }
+
+  readWordBool () {
+    return !!this.readWord()
+  }
+
+  readDouble () {
+    const val = this.buffer.readDoubleLE(this.offset)
+    this.offset += 8
+    return val
+  }
+}
+
+class FileHeader extends Block {
+  constructor (buffer, offset) {
+    super(buffer, offset)
 
     if (buffer.length - offset < 60) {
       throw new Error('Buffer is not large enough to hold header')
     }
 
-    this.OcadMark = buffer.readInt16LE(offset)
+    this.ocadMark = this.readSmallInt()
+    this.fileType = this.readByte()
+    this.offset++ // FileStatus, not used
+    this.version = this.readSmallInt()
+    this.subVersion = this.readByte()
+    this.subSubVersion = this.readByte()
+    this.symbolIndexBlock = this.readCardinal()
+    this.objectIndexBlock = this.readCardinal()
   }
 
   isValid () {
-    return this.OcadMark === 0x0cad
+    return this.ocadMark === 0x0cad
+  }
+}
+
+class ObjectIndex extends Block {
+  constructor (buffer, offset) {
+    super(buffer, offset)
+
+    this.nextObjectIndexBlock = this.readInteger()
+    this.table = new Array(256)
+    for (let i = 0; i < 256; i++) {
+      const rc = new LRect(buffer, this.offset)
+      this.offset += rc.size()
+
+      this.table[i] = {
+        rc,
+        pos: this.readInteger(),
+        len: this.readInteger(),
+        sym: this.readInteger(),
+        objType: this.readByte(),
+        encryptedMode: this.readByte(),
+        status: this.readByte(),
+        viewType: this.readByte(),
+        color: this.readSmallInt(),
+        group: this.readSmallInt(),
+        impLayer: this.readSmallInt(),
+        dbDatasetHash: this.readByte(),
+        dbKeyHash: this.readByte()
+      }
+    }
+  }
+
+  parseObjects () {
+    return this.table
+      .filter(o => o.pos)
+      .map(o => this.parseObject(o))
+  }
+
+  parseObject (objIndex) {
+    return {}
+  }
+}
+
+class LRect extends Block {
+  constructor (buffer, offset) {
+    super(buffer, offset)
+    this.min = {
+      x: this.readInteger(),
+      y: this.readInteger()
+    }
+    this.max = {
+      x: this.readInteger(),
+      y: this.readInteger()
+    }
+  }
+
+  size () {
+    return 16
   }
 }
--- test/spec.js
+++ test/spec.js
@@ -9,5 +9,15 @@ test('too small files can not be opened', async t => {
 })
 
 test('can open valid file', async t => {
-  await t.notThrows(ocad2geojson(path.join(__dirname, 'data', 'Tuve_Databas_075.ocd')))
+  const map = await ocad2geojson(path.join(__dirname, 'data', 'Tuve_Databas_075.ocd'))
+  t.is(map.header.version, 12, 'Version mismatch')
+  t.is(map.header.subVersion, 2, 'Subversion mismatch')
+  t.is(map.header.subSubVersion, 0, 'Subsubversion mismatch')
+  t.is(map.header.symbolIndexBlock, 9928, 'First symbol index block')
+  t.is(map.header.objectIndexBlock, 199488, 'First object index block')
+})
+
+test('can read objects from file', async t => {
+  const map = await ocad2geojson(path.join(__dirname, 'data', 'Tuve_Databas_075.ocd'))
+  t.is(map.objects.length, 15552)
 })

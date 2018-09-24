cd93c39087ee49d251d176696a0dd9208b74eebe
Per Liedman <per.liedman@dotnetmentor.se>
2018-09-24 11:44:24 +0200

    Real world coordinates; parameter strings; CLI args

--- cli.js
+++ cli.js
@@ -1,4 +1,41 @@
+const fs = require('fs')
+const argv = require('minimist')(process.argv.slice(2));
 const ocad2geojson = require('./')
+const filePath = argv._[0]
+const outStream = argv.o ? fs.createWriteStream(argv.o) : process.stdout
 
-ocad2geojson(process.argv[2])
-  .then(map => console.log(JSON.stringify({type: 'FeatureCollection', features: map.objects}, null, 2)))
+ocad2geojson(filePath)
+  .then(map => {
+    if (argv.v) {
+      writeInfo(filePath, map, process.stderr)
+    }
+
+    if (!argv.p) {
+      outStream.write(JSON.stringify(map.featureCollection, null, 2))
+    } else {
+      outStream.write(JSON.stringify(map.parameterStrings, null, 2))
+    }
+  })
+
+const writeInfo = (path, map, stream) => {
+  const header = map.header
+
+  let infos = [
+    `File: ${path}`,
+    `OCAD version: ${header.version}.${header.subVersion}.${header.subSubVersion}`,
+    `File version: ${header.currentFileVersion}`,
+    `Total number objects: ${map.featureCollection.features.length}`,
+  ]
+
+  if (map.parameterStrings[1039]) {
+    const scalePar = map.parameterStrings[1039][0]
+    infos = infos.concat([
+      `Scale: 1:${scalePar.m}`,
+      `Northing: ${scalePar.y}`,
+      `Easting: ${scalePar.x}`
+    ])
+  }
+
+  stream.write(infos.join('\n'))
+  stream.write('\n')
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 const { Buffer } = require('buffer')
+const { coordEach } = require('@turf/meta')
 
-module.exports = async path => {
+module.exports = async (path, options) => {
+  options = options || {}
+
   if (Buffer.isBuffer(path)) {
-    return parseOcadBuffer(path)
+    return parseOcadBuffer(path, options)
   } else {
     const buffer = await new Promise((resolve, reject) =>
       fs.readFile(path, (err, buffer) => {
@@ -11,11 +14,11 @@ module.exports = async path => {
 
         resolve(buffer)
       }))
-    return parseOcadBuffer(buffer)
+    return parseOcadBuffer(buffer, options)
   }
 }
 
-const parseOcadBuffer = async buffer => new Promise((resolve, reject) => {
+const parseOcadBuffer = async (buffer, options) => new Promise((resolve, reject) => {
   const header = new FileHeader(buffer)
   if (!header.isValid()) {
     reject(new Error(`Not an OCAD file (invalid header ${header.ocadMark} !== ${0x0cad})`))
@@ -30,11 +33,56 @@ const parseOcadBuffer = async buffer => new Promise((resolve, reject) => {
     objectIndexOffset = objectIndex.nextObjectIndexBlock
   }
 
+  let featureCollection = {
+    type: 'FeatureCollection',
+    features: objects
+  }
+
+  let parameterStrings = {}
+  let stringIndexOffset = header.stringIndexBlock
+  while (stringIndexOffset) {
+    let stringIndex = new StringIndex(buffer, stringIndexOffset)
+    const strings = stringIndex.getStrings()
+
+    Object.keys(strings).reduce((a, recType) => {
+      const typeStrings = strings[recType]
+      let concatStrings = a[recType] || []
+      a[recType] = concatStrings.concat(typeStrings.map(s => s.values))
+      return a
+    }, parameterStrings)
+
+    stringIndexOffset = stringIndex.nextStringIndexBlock
+  }
+
+  if (parameterStrings['1039'] && (options.applyCrs || options.applyCrs === undefined)) {
+    applyCrs(featureCollection, parameterStrings['1039'][0])
+  }
+
   resolve({
     header,
-    objects
+    parameterStrings,
+    featureCollection
   })
 })
+
+const applyCrs = (featureCollection, scalePar) => {
+  // OCAD uses 1/100 mm of "paper coordinates" as units, we
+  // want to convert to meters in real world
+  const hundredsMmToMeter = 1 / (100 * 1000)
+  let { x, y, m } = scalePar
+
+  // Easting (meters)
+  x = Number(x)
+  // Northing (meters)
+  y = Number(y)
+  // Map scale
+  m = Number(m)
+
+  coordEach(featureCollection, coord => {
+    coord[0] = (coord[0] * hundredsMmToMeter) * m + x
+    coord[1] = (coord[1] * hundredsMmToMeter) * m + y
+  })
+}
 
 class Block {
   constructor (buffer, offset) {
@@ -93,12 +141,23 @@ class FileHeader extends Block {
 
     this.ocadMark = this.readSmallInt()
     this.fileType = this.readByte()
-    this.offset++ // FileStatus, not used
+    this.readByte() // FileStatus, not used
     this.version = this.readSmallInt()
     this.subVersion = this.readByte()
     this.subSubVersion = this.readByte()
     this.symbolIndexBlock = this.readCardinal()
     this.objectIndexBlock = this.readCardinal()
+    this.offlineSyncSerial = this.readInteger()
+    this.currentFileVersion = this.readInteger()
+    this.readCardinal() // Internal, not used
+    this.readCardinal() // Internal, not used
+    this.stringIndexBlock = this.readCardinal()
+    this.fileNamePos = this.readCardinal()
+    this.fileNameSize = this.readCardinal()
+    this.readCardinal() // Internal, not used
+    this.readCardinal() // Res1, not used
+    this.readCardinal() // Res2, not used
+    this.mrStartBlockPosition = this.readCardinal()
   }
 
   isValid () {
@@ -221,8 +280,8 @@ class TObject extends Block {
 
     for (let i = 0; i < this.nItem; i++) {
       this.coordinates[i] = [
-        ((this.readInteger() & 0xffffff00) >> 8) * 0.01,
-        ((this.readInteger() & 0xffffff00) >> 8) * 0.01
+        (this.readInteger() >> 8),
+        (this.readInteger() >> 8)
       ]
     }
   }
@@ -247,6 +306,60 @@ class TObject extends Block {
       nDatabaseString: this.nDatabaseString,
       objectStringType: this.objectStringType,
       res1: this.res1
+    }
+  }
+}
+
+class StringIndex extends Block {
+  constructor (buffer, offset) {
+    super(buffer, offset)
+
+    this.nextStringIndexBlock = this.readInteger()
+    this.table = new Array(256)
+    for (let i = 0; i < 256; i++) {
+      this.table[i] = {
+        pos: this.readInteger(),
+        len: this.readInteger(),
+        recType: this.readInteger(),
+        objIndex: this.readInteger()
+      }
+    }
+  }
+
+  getStrings () {
+    const strings = this.table
+      .filter(si => si.recType > 0)
+      .map(si => new ParameterString(this.buffer, si.pos, si))
+    return strings.reduce((pss, ps) => {
+      let typeStrings = pss[ps.recType]
+      if (!typeStrings) {
+        pss[ps.recType] = typeStrings = []
+      }
+
+      typeStrings.push(ps)
+
+      return pss
+    }, {})
+  }
+}
+
+class ParameterString extends Block {
+  constructor (buffer, offset, indexRecord) {
+    super(buffer, offset)
+
+    this.recType = indexRecord.recType
+
+    let val = ''
+    let nextByte = 0
+    for (let i = 0; i < indexRecord.len && (nextByte = this.readByte()); i++) {
+      val += String.fromCharCode(nextByte)
+    }
+
+    const vals = val.split('\t')
+    this.first = vals[0]
+    this.values = {}
+    for (let i = 1; i < vals.length; i++) {
+      this.values[vals[i][0]] = vals[i].substring(1)
     }
   }
 }
